ff1d994c7922ff23d2425af7007867f324f283f9
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-04 15:56:15 -0500

    add loop to function concatenate string to var a given number of times return var

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(returnedString, repeat) {
+for (let i = 1; i < repeat; i++) {
+    returnedString += `hey`;
+}
+return returnedString;
 };
 
 // Do not edit below this line
