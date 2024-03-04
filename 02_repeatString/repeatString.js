32f1025d49c4b7cbc0d2d728e6056e3261f1200a
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-04 16:34:10 -0500

    add handling for negative input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(storedString, repeat) {
-let returnedString = '';
-    for (let i = 0; i < repeat; i++) {
-        returnedString += storedString;
+    let returnedString = '';
+    if (Math.sign(repeat) == -1) {
+        returnedString = 'ERROR';
+        return returnedString;
     }
-return returnedString;
+    for (let i = 0; i < repeat; i++) {
+            returnedString += storedString;
+    }
+    return returnedString;
 };
 
 // Do not edit below this line
