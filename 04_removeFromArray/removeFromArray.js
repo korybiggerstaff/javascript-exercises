2a2b48d42ba7eef4ec38b6a74e1d4f1589a6a949
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-04 19:54:21 -0500

    add functionality to removeFromArray; first attempt uses splice()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(fullArray, removeElement) {
+    for (let i = 0; i < fullArray.length; i++) {
+        if (removeElement == fullArray[i]){
+            fullArray.splice(i, 1);
+        }
+    }
 };
 
 // Do not edit below this line
