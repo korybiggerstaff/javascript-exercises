c571a8747fd194db40c81fbe17302897b5a663c4
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-04 19:33:22 -0500

    add reverseString functionality, first attempt

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(beginningString) {//take a string, split it into elements in an array, reverse the array, then join the elements back together.
+    const stringToArray = beginningString.split(""); 
+    stringToArray.reverse();
+    return stringToArray.join("");
 };
 
 // Do not edit below this line
