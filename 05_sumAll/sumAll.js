48ba70b4183e1e32a956f2871b01881c9148abcc
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-11 22:34:03 -0400

    add check for negative values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,6 +4,9 @@ let min;
 let max;
 min = Math.min(first,second);
 max = Math.max(first,second);
+if (Math.sign(first) == -1 || Math.sign(second) == -2) {
+    return "ERROR";
+}
 for (let i = min; i < max + 1; i++) {
     workingSum += i;
 }
