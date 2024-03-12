7d816c4faff8d57a40fe1d6098039ffa8f0167fc
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-11 22:16:15 -0400

    add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,11 @@
 const removeFromArray = function(inputArray, ...removedElement) {
-    const returnedArray = [];
-    inputArray.forEach((item) => {
-        if (!removedElement.includes(item)) {
-            returnedArray.push(item);
+    const returnedArray = []; //make a new array,
+    inputArray.forEach((item) => { //take each element of the old array,
+        if (!removedElement.includes(item)) { //check if the element is absent in the function arguments,
+            returnedArray.push(item); //and, if it is absent, push it to our new array.
         }
     });
-    return returnedArray;
+    return returnedArray; //return new array with only the elements we want to keep
 }
 
 // Do not edit below this line
