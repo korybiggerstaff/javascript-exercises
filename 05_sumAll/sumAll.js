a09bca0403556c6e1c212fb1a5d5841350b0b05c
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-11 22:37:24 -0400

    add check for NaN values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,7 +4,10 @@ let min;
 let max;
 min = Math.min(first,second);
 max = Math.max(first,second);
-if (Math.sign(first) == -1 || Math.sign(second) == -2) {
+if (Math.sign(first) == -1 ||
+    Math.sign(second) == -2 ||
+    isNaN(first) ||
+    isNan(second)) {
     return "ERROR";
 }
 for (let i = min; i < max + 1; i++) {
