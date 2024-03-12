baab072f1603e64869d270b8db14f0515b71415d
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-11 22:24:39 -0400

    try loop method for summing range of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+let workingSum = 0;
+for (let i = min; i < max + 1; i++) {
+    workingSum += i;
+}
+return workingSum;
 };
 
 // Do not edit below this line
