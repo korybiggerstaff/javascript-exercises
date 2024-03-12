90a82735ecd53b0b25f8bb2ad5a0d52347a352ad
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-11 22:31:21 -0400

    add "min" and "max" variables: we can input min and max values in either order

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function(min, max) {
+const sumAll = function(first, second) {
 let workingSum = 0;
+let min;
+let max;
+min = Math.min(first,second);
+max = Math.max(first,second);
 for (let i = min; i < max + 1; i++) {
     workingSum += i;
 }
