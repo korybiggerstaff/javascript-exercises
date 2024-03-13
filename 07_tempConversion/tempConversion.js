ff994abae2a661afc8cabb33988c2dca9f7b98d3
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-12 20:26:46 -0400

    change Math.floor to Math.round; was always rounding down

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(fTemp) {
-return Math.floor(((fTemp - 32) * (5 / 9)) * 10) / 10;
+return Math.round(((fTemp - 32) * (5 / 9)) * 10) / 10;
 };
 
 const convertToFahrenheit = function(cTemp) {
-return Math.floor (((cTemp * 1.8) + 32) * 10) / 10;
+return Math.round(((cTemp * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
