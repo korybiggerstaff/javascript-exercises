35d1bfe565075ea470b33b66c863468d1e7af6ff
korybiggerstaff <biggerstaff.k@gmail.com>
2024-03-12 20:25:25 -0400

    add basic logic for convertToCelsius function add basic logic for convertToFahrenheit function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+return Math.floor(((fTemp - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+return Math.floor (((cTemp * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
